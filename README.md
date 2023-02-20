c897e1298c583b417f31655a68c7e932e856c624
salespaulo <salespaulo08@gmail.com>
2023-02-20 11:42:50 -0300

    Primeiros passos e configuracoes do projeto

---
+++ README.md
@@ -0,0 +1,27 @@
+# Wipro - Busca endereço por CEP
+
+Teste da empresa Wipro onde devemos consultar o valor do frete com base na região, durante a busca do endereço pelo CEP oferecido.
+
+## Ferramentas
+
+- [Spring](./HELP.md)
+- [Lombok](https://projectlombok.org/setup/eclipse)
+
+
+## Executando
+
+Para executar devemos ter a máquina virtual do java instalada no servidor na versão *jre 11*.
+
+**Linux**
+
+```bash
+./run.sh
+
+```
+
+**Windows**
+
+```bash
+./run.sh
+
+```
--- src/main/java/br/com/wipro/cep/search/CepSearchApplication.java
+++ src/main/java/br/com/wipro/cep/search/CepSearchApplication.java
@@ -2,12 +2,20 @@ package br.com.wipro.cep.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class CepSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CepSearchApplication.class, args);
+		healthy(SpringApplication.run(CepSearchApplication.class, args));
+	}
+
+	private static void healthy(ConfigurableApplicationContext context) {
+		log.info("Running Application ID {} - Successfuly!", context.getId());
 	}
 
 }
