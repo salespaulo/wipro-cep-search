f6724f6a93d7c915164ad1c77770e3ed47e4ea51
salespaulo <salespaulo08@gmail.com>
2023-02-20 10:50:22 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/wipro/cep/search/CepSearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.wipro.cep.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wipro/cep/search/CepSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wipro.cep.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
